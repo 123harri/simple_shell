bb56dc73fa89fbcbfcb9239532abc20fbe9ed575
larinegit <larineomoit04@gmail.com>
2023-11-10 10:08:32 -0800

    atoi

---
+++ change_int.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @inf: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(inf_t *inf)
+{
+	return (isatty(STDIN_FILENO) && inf->readla <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
